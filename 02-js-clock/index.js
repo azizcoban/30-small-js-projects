a51bdaa30cc956eb6c814f44105b5ea36f26a139
Aziz Çoban <aziizcoban@gmail.com>
2021-07-24 00:55:49 +0300

    js analog clock developed.

---
+++ 02-js-clock/index.js
@@ -0,0 +1,27 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+const setDate = () => {
+  const date = new Date();
+  const seconds = date.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+
+  const mins = date.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + 90;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`
+
+
+  const hours = date.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+
+
+}
+
+setInterval(() => {
+  setDate();
+}, 1000)
